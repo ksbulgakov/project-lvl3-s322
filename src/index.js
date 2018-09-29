bf19df61bdd990634b0f2171658faa7fc6e118b1
ksbulgakov <ksbulgakov@gmail.com>
2018-09-29 17:57:52 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -5,53 +5,59 @@ import cheerio from 'cheerio';
 import axios from './lib/axiosAdapted';
 import { makeLocalLink, makeFileName, makeFolderName } from './makeName';
 
+const handleHtnlData = (res, host) => {
+  const $ = cheerio.load(res.data);
+
+  const tags = {
+    img: 'src',
+    link: 'href',
+    script: 'src',
+  };
+
+  const links = Object.keys(tags).reduce((acc, tag) => {
+    let originalPaths = [];
+    $(tag).each(function findLink() {
+      const link = $(this).attr(tags[tag]);
+
+      if (!link || url.parse(link).host) {
+        return;
+      }
+
+      originalPaths = [...originalPaths, path.normalize(`/${link}`)];
+      // Change link to local
+      $(this).attr(tags[tag], makeLocalLink(host, link));
+    });
+
+    return [...acc, ...originalPaths];
+  }, []);
+
+  return { htmlDocument: $.html(), links };
+};
 
 const loadPage = (host, dir = '') => {
   if (!host) {
     throw new Error('No link. Link is requared.');
   }
 
+  let linksList;
+
   return axios.get(host)
-    .then((res) => {
-      const $ = cheerio.load(res.data);
-
-      const tags = {
-        img: 'src',
-        link: 'href',
-        script: 'src',
-      };
-
-      const linksList = Object.keys(tags).reduce((acc, tag) => {
-        let links = [];
-        $(tag).each(function findLink() {
-          const link = $(this).attr(tags[tag]);
-
-          if (!link || url.parse(link).host) {
-            return;
-          }
-
-          links = [...links, path.normalize(`/${link}`)];
-          // Change link to local
-          $(this).attr(tags[tag], makeLocalLink(host, link));
-        });
-
-        return [...acc, ...links];
-      }, []);
-
-      fsPromises.writeFile(`${path.resolve(dir)}/${makeFileName(host)}`, $.html());
-
-      if (linksList.length > 0) {
-        fsPromises.mkdir(`${path.resolve(dir)}/${makeFolderName(host)}`)
-          .then(() => linksList.forEach(link =>
-            axios({
-              method: 'get',
-              url: `${host}${link}`,
-              responseType: 'arraybuffer',
-            })
-              .then(response => fsPromises
-                .writeFile(`${path.resolve(dir)}/${makeLocalLink(host, link)}`, response.data, 'utf8'))));
-      }
-    });
+    .then(res => handleHtnlData(res, host))
+    .then((data) => {
+      linksList = data.links;
+      return fsPromises.writeFile(`${path.resolve(dir)}/${makeFileName(host)}`, data.htmlDocument);
+    })
+    .then(() => fsPromises.mkdir(`${path.resolve(dir)}/${makeFolderName(host)}`))
+    .then(() => Promise.all(linksList.map(link =>
+      axios({
+        method: 'get',
+        url: `${host}${link}`,
+        responseType: 'arraybuffer',
+      })
+        .then((response) => {
+          console.log(response);
+          return fsPromises.writeFile(`${path.resolve(dir)}/${makeLocalLink(host, link)}`, response.data, 'utf8');
+        }))));
 };
 
 export default loadPage;
